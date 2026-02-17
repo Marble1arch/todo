8f130a9d36c1ea20480f942ca82e5d4a5bd78ac7
ipb24.r.terauds <ipb24.r.terauds@vtdt.edu.lv>
2026-02-17 15:13:48 +0200

    l

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/why', function () {
+    return view('why');
+});
+Route::get('/background', function () {
+    return view('background');
+});
+Route::get('/Creative', function () {
+    return view('creative');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
